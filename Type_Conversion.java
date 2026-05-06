da60ee1cec799274f7c74542fd01e638aafdd152
Youbraj <youbrajthapa349@example.com>
2026-05-06 15:41:45 +0545

    update

---
+++ Type_Conversion.java
@@ -0,0 +1,21 @@
+/*
+i) Type Conversion
+-> Type conversion is the process of changing a value from one data type to another.
+
+ii) Type Casting
+-> Type casting is the explicit (manual) form of type conversion where the programmer forces a value to be
+ treated as a different data type.Type conversion happens automatically when converting smaller data types to larger ones, 
+ while type casting is required when converting larger data types to smaller ones.
+
+*/
+
+
+public class Type_Conversion {
+    public static void main(String[] args) {
+        int age = 4;
+        int num = age; //--------> type coversion
+        float  a = 33.00f;
+int b = (int) a; //-------------> type casting
+        System.out.println(b);
+    }
+}
