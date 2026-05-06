c76d0cf8f031a38d34f319a5592e7f17d9ac7305
Youbraj Thapa <youbrajthapa349@gmail.com>
2026-05-06 18:27:18 +0545

    Update B_Variable.java

--- B_Variable.java
+++ B_Variable.java
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-/*
-A variable is a named memory location used to store data values that can be changed during program execution in Java.
 
-Types of variable :
-1)local variable
--> A local variable is a variable declared inside a method and is accessible only within that method. It must be initialized before use.
-
-2) instance variable
--> An instance variable is declared inside a class but outside methods and is accessed using an object. It gets a default value if not initialized.
-
-*/
-
-
-
-public class B_Variable {
-   int number = 33; // instance variable
-
-    void display() {
-        int Num = 33; // local variable
-        System.out.println("number : " + number);
-        System.out.println("Num : " + Num);
-    }
-
-    public static void main(String[] args) {
-        B_Variable obj = new B_Variable();
-        obj.display();
-    }
-}
-=======
 /*
 A variable is a named memory location used to store data values that can be changed during program execution in Java.
 
